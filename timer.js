d326aa7c8c9c7cceffda383d374649d7902827cc
Marui <152158429+abc-abc-ab@users.noreply.github.com>
2025-07-19 09:06:15 +0900

    Initial Commit

--- timer.js
+++ timer.js
@@ -1,36 +1,41 @@
 'use strict';
-/** 関数
- *  @function Number.prototype.PadTo2Digits
- *  @memberof Number.prototype
- *  @returns {string}
- */
-Object.defineProperty(Number.prototype, "PadTo2Digits", {
-    value: /** @this {Number}*/function (){return String(this).padStart(2, "0"); },
-    writable: false
-});
-((d, t)=>{
-    let count = 0, param = new URLSearchParams(d.location.href), time = Number(param.get("time") || 0);
+const d = document, t = window;
+let interval = 0;
+
+window.addEventListener("DOMContentLoaded", ((e)=>{
+    let count = 0, param = new URLSearchParams(d.location.href);
+    const Traningtime = param.get("time") * 60, breakTime = param.get("break") * 60;
+    let time = 0;
     const contentElement = d.getElementById("content"),
     timeElement = d.getElementById("time"),
     countElement = d.getElementById("count");
-    if (time === null){d.location.href = "./"}
+    if (isNaN(Traningtime) || isNaN(breakTime)){d.location.href = "./"}
+
+    /** 関数
+     * @function Number.prototype.PadTo2Digits
+     * @memberof Number.prototype
+     * @returns {string}
+     */
+    Object.defineProperty(Number.prototype, "PadTo2Digits",
+        { value: /** @this {Number}*/function() {return String(this).padStart(2, "0"); }, writable: false
+    });
 
     t.addEventListener("click", (e)=>{
         countElement.innerText = ++count;
     })
-    t.addEventListener("beforeunload", (e)=>{history.replaceState("", "", "../timer.html")})
-    t.setInterval(CountDown, 1, [timeElement, time])
+    interval = t.setInterval(CountDown, 10, [timeElement, time]);
 
     //
-
-    history.replaceState("", "", "./timer.html/#")
-})(document, window)
+}));
 
 /** It's count down function.
  * @param {HTMLElement} elm 
  * @param {Number} time 
  */
-function CountDown(elm, time){
+function CountDown(elm, time) {
     time -= 0.01
-    elm.innerText = `${Math.floor(time / 60).PadTo2Digits()}:${(Math.trunc(time) % 60).PadTo2Digits()}.00`;
+    elm.innerText =
+    `${Math.floor(Math.trunc(time) / 60).PadTo2Digits()}:${
+     (Math.trunc(time) % 60).PadTo2Digits()}.${
+     ((time - Math.trunc(time)) * 100).PadTo2Digits()}`;
 }
